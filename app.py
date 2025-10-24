eba567a5753f1b64a5d66baca4ac63ebf8de226c
varun-chourasia <chourasiavarun16@gmail.com>
2025-10-24 19:39:36 +0530

    update port in app.py

--- app.py
+++ app.py
@@ -79,5 +79,5 @@ def internal_error(e):
 
 # Entry point for Render
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Render sets PORT automatically
+    port = int(os.environ('PORT')) 
     app.run(host='0.0.0.0', port=port)
